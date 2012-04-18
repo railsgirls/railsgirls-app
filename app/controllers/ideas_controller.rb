6dbda3e9af7b0f857360aa0b04e3f89b545daaad
Terence Lee <hone02@gmail.com>
2012-04-18 13:40:49 +0200

    2. create idea scaffold

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,83 @@
+class IdeasController < ApplicationController
+  # GET /ideas
+  # GET /ideas.json
+  def index
+    @ideas = Idea.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @ideas }
+    end
+  end
+
+  # GET /ideas/1
+  # GET /ideas/1.json
+  def show
+    @idea = Idea.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @idea }
+    end
+  end
+
+  # GET /ideas/new
+  # GET /ideas/new.json
+  def new
+    @idea = Idea.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @idea }
+    end
+  end
+
+  # GET /ideas/1/edit
+  def edit
+    @idea = Idea.find(params[:id])
+  end
+
+  # POST /ideas
+  # POST /ideas.json
+  def create
+    @idea = Idea.new(params[:idea])
+
+    respond_to do |format|
+      if @idea.save
+        format.html { redirect_to @idea, notice: 'Idea was successfully created.' }
+        format.json { render json: @idea, status: :created, location: @idea }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /ideas/1
+  # PUT /ideas/1.json
+  def update
+    @idea = Idea.find(params[:id])
+
+    respond_to do |format|
+      if @idea.update_attributes(params[:idea])
+        format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /ideas/1
+  # DELETE /ideas/1.json
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+
+    respond_to do |format|
+      format.html { redirect_to ideas_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ test/functional/ideas_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class IdeasControllerTest < ActionController::TestCase
+  setup do
+    @idea = ideas(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:ideas)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create idea" do
+    assert_difference('Idea.count') do
+      post :create, idea: @idea.attributes
+    end
+
+    assert_redirected_to idea_path(assigns(:idea))
+  end
+
+  test "should show idea" do
+    get :show, id: @idea
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @idea
+    assert_response :success
+  end
+
+  test "should update idea" do
+    put :update, id: @idea, idea: @idea.attributes
+    assert_redirected_to idea_path(assigns(:idea))
+  end
+
+  test "should destroy idea" do
+    assert_difference('Idea.count', -1) do
+      delete :destroy, id: @idea
+    end
+
+    assert_redirected_to ideas_path
+  end
+end
