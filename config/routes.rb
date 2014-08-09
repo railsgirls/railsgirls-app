286ccf973b551c16067334d3c747c82171ffb53b
H. I. Lee <hilee@mcHuki.local>
2014-08-09 16:59:17 +0800

    Replacing "Ideas" with "Photo Gallery"; the Idea model, controller & view files are retained

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Railsgirls::Application.routes.draw do
+  resources :photos
   resources :ideas
-  root :to => redirect("/ideas")
+  root :to => redirect("/photos")
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
